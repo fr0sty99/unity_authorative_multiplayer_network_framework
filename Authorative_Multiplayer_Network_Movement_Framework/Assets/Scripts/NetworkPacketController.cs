ab9dbc78ea3c40d52dac681d0d55d98eb97edd2b
fr0sty99 <j.neber@gmx.ch>
2018-10-12 12:28:30 +0200

    added files. WIP

---
+++ Authorative_Multiplayer_Network_Movement_Framework/Assets/Scripts/NetworkPacketController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class NetworkPacketController : NetworkBehaviour {
+    [System.Serializable]
+    public class Package {
+        public float horizontal;
+        public float vertical;
+        public float timeStamp;
+    }
+}
---
+++ Authorative_Multiplayer_Network_Movement_Framework/Assets/Scripts/PlayerNetwork.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerNetwork : MonoBehaviour
+{
+    [SerializeField]
+    float moveSpeed;
+
+    CharacterController controller;
+
+    // Use this for initialization
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        LocalClientUpdate();
+    }
+
+    void LocalClientUpdate() {
+        Move(Input.GetAxis("Horizontal") * moveSpeed, Input.GetAxis("Vertical") * moveSpeed);
+    }
+
+    void Move(float horizontal, float vertical)
+    {
+        controller.Move(new Vector3(horizontal, 0, vertical));
+    }
+}
