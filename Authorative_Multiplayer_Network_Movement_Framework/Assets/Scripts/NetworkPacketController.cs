03d80d5704f3d0ed84c71cb5c5f43b095fa4cdd3
fr0sty99 <j.neber@gmx.ch>
2018-10-13 03:54:43 +0200

    added classes. WIP

--- Authorative_Multiplayer_Network_Movement_Framework/Assets/Scripts/NetworkPacketController.cs
+++ Authorative_Multiplayer_Network_Movement_Framework/Assets/Scripts/NetworkPacketController.cs
@@ -1,13 +1,75 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿
 using UnityEngine.Networking;
+using System;
 
-public class NetworkPacketController : NetworkBehaviour {
-    [System.Serializable]
+// this class is responsible for sending and receiving packages in out network
+public class NetworkPackageController : NetworkBehaviour {
+    [Serializable]
     public class Package {
         public float horizontal;
         public float vertical;
         public float timeStamp;
+    }
+
+    [Serializable]
+    public class ReceivePackage {
+        public float x;
+        public float y;
+        public float z;
+        public float timeStamp;
+    }
+
+    private NetworkPackageManager<Package> m_PackageManager;
+    public NetworkPackageManager<Package> PackageManager {
+        get {
+            if(m_PackageManager == null) {
+                m_PackageManager = new NetworkPackageManager<Package>();
+                if(isLocalPlayer) {
+                    m_PackageManager.OnRequirePackageTransmit += TransmitPackageToServer;
+                }
+            }
+            return m_PackageManager;
+        }
+    }
+
+    private NetworkPackageManager<ReceivePackage> m_ServerPackageManager;
+    public NetworkPackageManager<ReceivePackage> ServerPackageManager
+    {
+        get
+        {
+            if (m_ServerPackageManager == null)
+            {
+                m_ServerPackageManager = new NetworkPackageManager<ReceivePackage>();
+                if (isServer)
+                {
+                    m_ServerPackageManager.OnRequirePackageTransmit += TransmitPackageToClient;
+                }
+            }
+            return m_ServerPackageManager;
+        }
+    }
+
+    private void TransmitPackageToClient(byte[] bytes) {
+        RpcReceiveDataOnClient(bytes);
+    }
+
+    private void TransmitPackageToServer(byte[] bytes)
+    {
+        CmdTransmitPackages(bytes);
+    }
+
+    [Command]
+    void CmdTransmitPackages(byte[] data) {
+        PackageManager.ReceiveData(data);
+    }
+
+    [ClientRpc]
+    void RpcReceiveDataOnClient(byte[] data) {
+        ServerPackageManager.ReceiveData(data);
+    }
+
+    public virtual void FixedUpdate() {
+        PackageManager.Tick();
+        ServerPackageManager.Tick();
     }
 }
--- Authorative_Multiplayer_Network_Movement_Framework/Assets/Scripts/PlayerNetwork.cs
+++ Authorative_Multiplayer_Network_Movement_Framework/Assets/Scripts/PlayerNetwork.cs
@@ -1,33 +1,108 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 [RequireComponent(typeof(CharacterController))]
-public class PlayerNetwork : MonoBehaviour
+public class PlayerNetwork : NetworkPackageController
 {
     [SerializeField]
     float moveSpeed;
 
+    [SerializeField]
+    [Range(0.1f, 1)]
+    float networkSendRate = .5f;
+
     CharacterController controller;
 
-    // Use this for initialization
+    List<ReceivePackage> predictedPackages;
+    Vector3 lastPosition;
+
     void Start()
     {
         controller = GetComponent<CharacterController>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
         LocalClientUpdate();
-    }
-
-    void LocalClientUpdate() {
-        Move(Input.GetAxis("Horizontal") * moveSpeed, Input.GetAxis("Vertical") * moveSpeed);
+        ServerUpdate();
+        RemoteClientUpdate();
     }
 
     void Move(float horizontal, float vertical)
     {
         controller.Move(new Vector3(horizontal, 0, vertical));
     }
+
+    void LocalClientUpdate()
+    {
+        if (!isLocalPlayer)
+            return;
+        if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
+        {
+            Debug.Log("Movement going on");
+            Debug.Log("Horizontal: " + Input.GetAxis("Horizontal"));
+            Debug.Log("Vertical: " + Input.GetAxis("Vertical"));
+            float time = Time.time;
+            PackageManager.AddPackage(new Package
+            {
+                horizontal = Input.GetAxis("Horizontal"),
+                vertical = Input.GetAxis("Vertical"),
+                timeStamp = time
+            });
+        }
+
+        //               Move(Input.GetAxis("Horizontal") * moveSpeed, Input.GetAxis("Vertical") * moveSpeed);
+
+    }
+
+    void ServerUpdate() {
+        Debug.Log("ServerUpdate");
+        if(!isServer || isLocalPlayer) {
+            return;
+        }
+        Debug.Log("authorizde");
+
+        Package packageData = PackageManager.GetNextDataReceived();
+        Debug.Log("PackageData: " + packageData);
+
+        if(packageData == null) {
+            return;
+        }
+
+        Move(packageData.horizontal * moveSpeed, packageData.vertical * moveSpeed);
+        Debug.Log("move horizontal: " + packageData.horizontal * moveSpeed);
+        Debug.Log("move vertical: " + packageData.vertical * moveSpeed);
+        if(transform.position == lastPosition) {
+            return;
+        }
+
+        lastPosition = transform.position;
+
+        ServerPackageManager.AddPackage(new ReceivePackage
+        {
+            x = transform.position.x,
+            y = transform.position.y,
+            z = transform.position.z,
+            timeStamp = packageData.timeStamp
+        });
+
+    }
+
+    public void RemoteClientUpdate() {
+        if(isServer) 
+            return;
+
+        var data = ServerPackageManager.GetNextDataReceived();
+
+        if (data == null)
+            return;
+
+        transform.position = new Vector3(data.x, data.y, data.z);
+        
+    }
+
 }
