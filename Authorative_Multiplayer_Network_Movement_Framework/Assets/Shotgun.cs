d121a4addd475b60dbfa556ecf91901934bc63fb
fr0sty99 <j.neber@gmx.ch>
2019-09-13 14:54:33 +0200

    wip

---
+++ Authorative_Multiplayer_Network_Movement_Framework/Assets/Shotgun.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Security.Cryptography;
+
+[RequireComponent(typeof(Transform))]
+[System.Serializable]
+public class Shotgun
+{
+    public string name = "Shotgun";
+    public GameObject gunPrefab;
+
+    public float fireRate = 1;
+    public int damage = 10;
+    public float range = 10f;
+    public Transform firePoint;
+}
